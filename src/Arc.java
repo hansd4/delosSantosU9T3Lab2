2ed590546387de40ba7f44516a608132bd8417b8
Hans de los Santos <hansd4@nycstudents.net>
2023-03-13 00:06:00 -0400

    final commit

--- src/Arc.java
+++ src/Arc.java
@@ -1,19 +1,37 @@
 import java.awt.Graphics;
 
 public class Arc extends Shape {
-    private int startAngle;
-    private int arcAngle;
+    private static int startAngle = 30;
+    private static int arcAngle = 70;
+    private int myStartAngle;
+    private int myArcAngle;
 
     public Arc() {
         super();
-        startAngle = 30;
-        arcAngle = 70;
+        myStartAngle = startAngle;
+        myArcAngle = arcAngle;
+    }
+
+    public static int getStartAngle() {
+        return startAngle;
+    }
+
+    public static int getArcAngle() {
+        return arcAngle;
+    }
+
+    public static void setStartAngle(int newStartAngle) {
+        startAngle = newStartAngle;
+    }
+
+    public static void setArcAngle(int newArcAngle) {
+        arcAngle = newArcAngle;
     }
 
     @Override
     public void draw(Graphics g) {
         g.setColor(getColor());
 
-        g.drawArc(getMinX(), getMinY(), getWidth(), getHeight(), 30, 70);
+        g.drawArc(getMinX(), getMinY(), getWidth(), getHeight(), myStartAngle, myArcAngle);
     }
 }
--- src/ButtonPanel.java
+++ src/ButtonPanel.java
@@ -1,7 +1,7 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.JPanel;
-import javax.swing.JButton;
+import java.awt.event.KeyAdapter;
+import javax.swing.*;
 
 /**
  * Class ButtonPanel:  holds buttons to pick the current shape and to clear the shapes.
@@ -11,6 +11,11 @@ public class ButtonPanel extends JPanel {
   private String[] shapeNames; // array of shapes that can be drawn
   private JButton clearButton;  // clear button
   private ShapeIComponent shapeComponent;
+  private JTextField startAngleField;
+  private JTextField arcAngleField;
+  private JLabel startAngleLabel;
+  private JLabel arcAngleLabel;
+  private JComponent[] arcFields = new JComponent[4];
   
   /** Constructor
    * @param shapeComponent   the shape component
@@ -30,10 +35,16 @@ public class ButtonPanel extends JPanel {
     for (String name: shapeNames) {
       currButton = new JButton(name);
       add(currButton); // add is an INHERITED METHOD from a class in JPanel's inheritance hierarchy!
-      currButton.addActionListener(new ActionListener() {
-        public void actionPerformed (ActionEvent e) {
-          if (shapeComponent != null) {
-            shapeComponent.setShape(e.getActionCommand());
+      currButton.addActionListener(e -> {
+        if (shapeComponent != null) {
+          shapeComponent.setShape(e.getActionCommand());
+
+          for (JComponent field : arcFields) {
+            if (name.equals("Arc")) {
+              field.setVisible(true);
+            } else {
+              field.setVisible(false);
+            }
           }
         }
       });
@@ -42,12 +53,44 @@ public class ButtonPanel extends JPanel {
     // create the clear button
     clearButton = new JButton("Clear");
     add(clearButton); // add is an INHERITED METHOD from a class in JPanel's inheritance hierarchy!
-    clearButton.addActionListener(new ActionListener() {
-      public void actionPerformed (ActionEvent e) {
-        if (shapeComponent != null) {
-          shapeComponent.clearShapes();
-        }
+    clearButton.addActionListener(e -> {
+      if (shapeComponent != null) {
+        shapeComponent.clearShapes();
       }
     });
+
+    startAngleField = new JTextField(Integer.toString(Arc.getStartAngle()), 3);
+    arcAngleField = new JTextField(Integer.toString(Arc.getArcAngle()), 3);
+    startAngleLabel = new JLabel("Start Angle: ");
+    arcAngleLabel = new JLabel("Arc Angle: ");
+    arcFields[0] = startAngleLabel;
+    arcFields[1] = startAngleField;
+    arcFields[2] = arcAngleLabel;
+    arcFields[3] = arcAngleField;
+    for (JComponent field : arcFields) {
+      add(field);
+      field.setVisible(false);
+    }
+    startAngleField.addActionListener(e -> {
+      String text = startAngleField.getText();
+      if (isNumeric(text)) {
+        Arc.setStartAngle(Integer.parseInt(text));
+      }
+    });
+    arcAngleField.addActionListener(e -> {
+      String text = arcAngleField.getText();
+      if (isNumeric(text)) {
+        Arc.setArcAngle(Integer.parseInt(text));
+      }
+    });
+  }
+
+  private boolean isNumeric(String s) {
+    for (int i = 0; i < s.length(); i++) {
+      if (!Character.isDigit(s.charAt(i))) {
+        return false;
+      }
+    }
+    return true;
   }
 }
